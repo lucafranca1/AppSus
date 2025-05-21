b7ccc88779df5e162d14b0b67a991d6aa30e7046
Luca França <luca.requiao@gmail.com>
2025-05-21 14:31:27 -0300

    Primeiro commit

---
+++ AppSus/src/test/java/com/example/AppSus/AppSusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AppSus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
