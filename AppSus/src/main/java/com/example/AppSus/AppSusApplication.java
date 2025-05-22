a340f79cfab40201739fdc49e95bfd7cb6e5f65f
Luca França <luca.requiao@gmail.com>
2025-05-21 22:33:03 -0300

    feito os models

--- AppSus/src/main/java/com/example/AppSus/AppSusApplication.java
+++ AppSus/src/main/java/com/example/AppSus/AppSusApplication.java
@@ -4,10 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AppSusApplication {
+public class AppSusApplication implements Runnable {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppSusApplication.class, args);
+	}
+
+	@Override
+	public void run() {
+		
+		
 	}
 
 }
